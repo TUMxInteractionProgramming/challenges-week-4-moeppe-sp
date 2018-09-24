77f6c4238a2e4f1edfabd1548f8929460be3c596
Maximilian Speicher <maximilian.speicher@tum.de>
2018-09-24 17:10:10 +0200

    Challenge 7 initial commit

---
+++ js/channels.js
@@ -0,0 +1,44 @@
+var yummy = {
+    name: 'Yummy',
+    createdOn: new Date('April 1, 2016'),
+    createdBy: 'minus.plus.yummy',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999,
+};
+
+var sevenContinents = {
+    name: 'SevenContinents',
+    createdOn: new Date('April 1, 2016'),
+    createdBy: 'minus.plus.yummy',
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999,
+};
+
+var killerApp = {
+    name: 'KillerApp',
+    createdOn: new Date('April 1, 2016'),
+    createdBy: 'minus.plus.yummy',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999,
+};
+
+var firstPersonOnMars = {
+    name: 'FirstPersonOnMars',
+    createdOn: new Date('April 1, 2016'),
+    createdBy: 'minus.plus.yummy',
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999,
+};
+
+var octoberfest = {
+    name: 'Octoberfest',
+    createdOn: new Date('April 1, 2016'),
+    createdBy: 'minus.plus.yummy',
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999,
+};
--- js/script.js
+++ js/script.js
@@ -1,32 +1,56 @@
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
 
+var currentChannel = {
+    name: 'SevenContinents',
+    createdOn: new Date('April 1, 2016'),
+    createdBy: 'minus.plus.yummy',
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999,
+};
+var currentLocation = {
+    latitude: 48.186948,
+    longitude: 11.574883,
+    what3words: 'schwellen.zeit.lippe'
+};
+
 /**
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
-function switchChannel(channelName) {
+function switchChannel(channelObject) {
+    currentChannel = channelObject;
     //Log the channel switch
-    console.log("Tuning in to channel", channelName);
+    console.log("Tuning in to channel", channelObject);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = '#' + channelObject.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/' + channelObject.createdBy + '" target="_blank"><strong>' + channelObject.createdBy + '</strong></a>';
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    let typeOfStar = channelObject.starred ? 'fas' : 'far';
+    $('#channel-star').removeClass("fas far");
+    $('#channel-star').addClass(typeOfStar);
+    //$('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelObject.name + ')').addClass('selected');
+
+
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    currentChannel.starred = !currentChannel.starred;
+    $('#channel-star').toggleClass("fas");
+    $('#channel-star').toggleClass("far");
+    $('#channels li:contains(' + currentChannel.name + ') span .fa-star').toggleClass("fas");
+    $('#channels li:contains(' + currentChannel.name + ') span .fa-star').toggleClass("far");
 }
 
 /**
